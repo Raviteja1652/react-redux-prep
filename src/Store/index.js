87d5e02130d049452fccb4aef999eeda71f388f8
Raviteja1652 <ravitejamrt.165@gmail.com>
2024-07-29 11:30:52 +0530

    working with multiple states

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,14 +1,23 @@
 import { createStore } from 'redux'
 
-const counterReducer = (state = {counter: 0}, action) => {
+const initialState = {counter: 0, toggle: true}
+const counterReducer = (state = initialState, action) => {
     if (action.type === 'INCREMENT') {
         return {
-            counter: state.counter + action.value
+            counter: state.counter + action.value,
+            toggle: state.toggle
         }
     }
     if (action.type === 'DECREMENT') {
         return {
-            counter: state.counter - 1
+            counter: state.counter - 1,
+            toggle: state.toggle
+        }
+    }
+    if (action.type === 'toggle') {
+        return {
+            counter: state.counter,
+            toggle: !state.toggle
         }
     }
 
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,8 +6,11 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch()
   const counter = useSelector(state => state.counter)
+  const toggle = useSelector(state => state.toggle)
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' })
+  };
 
   const incrementHandler = () => {
     dispatch({ type: 'INCREMENT', value: 5 })
@@ -18,12 +21,14 @@ const Counter = () => {
 
   return (
     <main className={classes.counter}>
+      {toggle && <>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
+      </>}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
