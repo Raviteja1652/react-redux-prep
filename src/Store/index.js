9ca630aa840b0fc08d52886549ee038ae7d7584c
Raviteja1652 <ravitejamrt.165@gmail.com>
2024-07-29 11:53:22 +0530

    using createSlice and configureStore

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,29 +1,51 @@
-import { createStore } from 'redux'
+// import { createStore, combineReducers } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit'
 
 const initialState = {counter: 0, toggle: true}
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + action.value,
-            toggle: state.toggle
-        }
-    }
-    if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1,
-            toggle: state.toggle
-        }
-    }
-    if (action.type === 'toggle') {
-        return {
-            counter: state.counter,
-            toggle: !state.toggle
-        }
-    }
 
-    return state;
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialState,
+    reducers: {
+        incrementCounter(state, action) {
+            state.counter = state.counter + action.value
+        },
+        decrementCounter(state) {
+            state.counter--
+        },
+        toggleCounter(state) {
+            state.toggle = !state.toggle
+        }
+    }
+})
 
-}
-const store = createStore(counterReducer)
+
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === 'INCREMENT') {
+//         return {
+//             counter: state.counter + action.value,
+//             toggle: state.toggle
+//         }
+//     }
+//     if (action.type === 'DECREMENT') {
+//         return {
+//             counter: state.counter - 1,
+//             toggle: state.toggle
+//         }
+//     }
+//     if (action.type === 'toggle') {
+//         return {
+//             counter: state.counter,
+//             toggle: !state.toggle
+//         }
+//     }
+
+//     return state;
+
+// };
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
 
 export default store;
